37ee892e4fe774f5cbdb68463912fc6e23118f7d
root <root@localhost.localdomain>
2017-11-27 12:26:36 +0530

    Program_to_greet_user

---
+++ program_for_greeting_user.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+# program : promt for users name anf greet that user by saying hello
+
+name = raw_input("What is your Name: ")                    # in python 3 we just need input, raw_input is for 2.7
+print ("Hello "+name+ " !!!")
