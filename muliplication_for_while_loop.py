b6887ddad845adc9bace3b46fd2773156fc9cb60
root <root@localhost.localdomain>
2017-12-13 12:14:13 +0530

    modified using function

--- muliplication_for_while_loop.py
+++ muliplication_for_while_loop.py
@@ -1,17 +1,28 @@
 #!/usr/bin/python
+
 ''' Program to print multiplication table using for and while loop'''
 
+def using_for(num1):
+    print("Output Using FOR loop:\n ")
+    for i in range(1,10):
+        print(i*num1)
 
-num1=int(input("Enter no. to print multiplication Table using for loop: "))
-print("Output Using FOR loop:\n ")
-for i in range(1,10):
-    print(i*num1)    
+def using_while(num2):
+    print("Output Using WHILE loop:\n ")
+    count=1
+    while count <=10:
+        print(num2*count)
+        count+=1
 
+def get_number():
+    num1=int(input("Enter no. to print multiplication number using FOR loop: "))
+    num2=int(input("Enter no. to print multiplication number using WHILE loop: "))
+    return num1,num2
+
+num1,num2=get_number()
+print(num1,num2)
+using_for(num1)
 print("------------")
+using_while(num2)
 
-num2=int(input("Enter no. to print multiplication Table using WHILE loop: "))
-print("Output Using WHILE loop:\n ")
-count=1
-while count <=10:
-    print(num2*count)
-    count+=1
+
