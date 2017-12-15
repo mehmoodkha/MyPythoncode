67c619a2433af3f7debf379a65708fac439106af
root <root@localhost.localdomain>
2017-12-15 23:13:01 +0530

    string printing using upper,lower,capital methods

---
+++ string_printing.py
@@ -0,0 +1,10 @@
+''' Program for string '''
+
+fname=input("Enter first Name: ")
+lname=input("Enter last Name: ")
+
+print("\nName : ",fname.lower(), ", Surname : ",lname.lower())
+drint("- ",fname.upper(), lname.upper())
+print("--------------------- ---------")
+print("- ",fname.capitalize(), lname.capitalize())
+
